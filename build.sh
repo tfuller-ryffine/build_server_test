7d9ff9c67dc405137a1801f1282cdb600ac11640
Tim Fuller <tim.fuller@ryffine.com>
2020-11-12 11:21:41 -0700

    Test

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@
 echo ------
 echo Install Docker
 echo ------
-sudo yum update -y
+which yum
 wait
-sudo yum install -y docker
-wait
-sudo service docker start
-wait
-sudo usermod -a -G docker ec2-user
-wait
+which apt-get
+# sudo yum update -y
+# wait
+# sudo yum install -y docker
+# wait
+# sudo service docker start
+# wait
+# sudo usermod -a -G docker ec2-user
+# wait
